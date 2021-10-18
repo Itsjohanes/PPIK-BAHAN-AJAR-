da982d9f770577af5d99ef2a0a2a458dc4c60784
Itsjohanes <71767003+Itsjohanes@users.noreply.github.com>
2021-10-18 18:21:55 +0700

    Add files via upload

---
+++ no 2.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args) {
+        int x = 20;
+        if(x == 10)
+        {
+           System.out.println("sepuluh");
+        }else
+        {
+            System.out.println("Bukan sepuluh");
+
+        }
+    }
+}
---
+++ no 3.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args) {
+        int x = 20;
+        if(x == 10)
+        {
+           System.out.println("sepuluh");
+        }else if (x == 20)
+        {
+            System.out.println("dua puluh");
+
+        }else 
+        { 
+           System.out.println("Bukan keduanya");
+        }
+    }
+}
